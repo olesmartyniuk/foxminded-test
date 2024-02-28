37479b5ea7c2b541dbb51581bc3e01e5e993f857
Oleksandr Martyniuk <omartyniuk@realchemy.com>
2024-02-27 19:52:24 -0800

    Implemented task with unit-tests

---
+++ RowMaxSum/Calculator.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace RowMaxSum;
+
+public static class Calculator
+{
+    public static int? GetRowWithMaxSum(string[] rows, out List<int> brokenRows)
+    {
+        var rowsWithSum = rows
+            .Select((row, index) => 
+            {
+                var sum = CalculateSum(row, out bool isValid);                                        
+                return new { Index = index, Sum = sum, IsValid = isValid };
+            })
+            .ToList();
+
+        brokenRows = rowsWithSum
+            .Where(row => !row.IsValid)
+            .Select(row => row.Index)
+            .ToList();
+
+        var validRows = rowsWithSum
+            .Where(row => row.IsValid);
+
+        if (!validRows.Any())
+        {
+            return null;
+        }
+
+        return validRows
+            .Aggregate((r1, r2) => r2.Sum > r1.Sum ? r2 : r1)
+            .Index;                
+    }       
+
+    private static double? CalculateSum(string row, out bool isValid)
+    {        
+        var parsedNumbers = row
+            .Split(',')
+            .Select(Parse);
+
+        if (parsedNumbers.Any(num => num == null || double.IsInfinity(num.Value)))
+        {
+            isValid = false;
+            return null;
+        }
+
+        isValid = true;
+        return parsedNumbers.Sum();
+    }
+
+    private static double? Parse(string number)
+    {
+        if (double.TryParse(
+            number,
+            NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint,
+            CultureInfo.InvariantCulture,
+            out double parsedNumber))
+        {
+            return parsedNumber;
+        }
+
+        return null;
+    }
+}
---
+++ RowMaxSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace RowMaxSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fileName = GetFileName(args);
+        var rows = File.ReadAllLines(fileName);
+        var maxRow = Calculator.GetRowWithMaxSum(rows, out var brokenRows);
+
+        Console.WriteLine($"Row with maximum sum: {maxRow}");
+        Console.WriteLine($"Broken rows: {string.Join(',', brokenRows)}");            
+    }
+
+    private static string GetFileName(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            return args[0];
+        }
+
+        Console.Write("Enter the filename: ");
+        return Console.ReadLine();            
+    }
+}
